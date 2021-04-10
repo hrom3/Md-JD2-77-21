156f8a4103ab8c54eb60b69d3ec54bbfdd095425
Aleksandr Ahramovich <al-hrom-3@mail.ru>
2021-04-10 09:39:14 +0300

    edit Main.java

--- src/main/java/by/bsuir/Main.java
+++ src/main/java/by/bsuir/Main.java
@@ -2,6 +2,12 @@ package by.bsuir;
 
 public class Main {
     public static void main(String[] args) {
+
+
+        //Ctrl+W - word checking
+        //Alt+Shift - multiple cursor
+        //Alt + Mouse movements - multiple rows checking
+
         System.out.print("Hello, ");
         System.out.print("Maven and");
         System.out.println(" GIT");
