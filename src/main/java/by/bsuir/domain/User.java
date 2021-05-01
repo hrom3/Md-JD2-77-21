b5741394077293f3bd6f2a7d3449ce5bd74c63a2
Aleksandr Ahramovich <al-hrom-3@mail.ru>
2021-05-01 19:00:10 +0300

    changed User class

--- src/main/java/by/bsuir/domain/User.java
+++ src/main/java/by/bsuir/domain/User.java
@@ -4,33 +4,36 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
 import java.sql.Timestamp;
-import java.util.Date;
+import java.sql.Date;
 import java.util.Objects;
 
 public class User {
-    /*Here we will store PK of m_users table*/
-    private Long id;
 
+    private Long id;
     private String name;
     private String surname;
     private Date birthDay;
-    private Gender gender = Gender.NOT_SELECTED;
+    private String login;
+    private Float weight;
+    private boolean isDeleted;
     private Timestamp created;
     private Timestamp changed;
-    private Float weight;
 
     public  User() {
     }
 
-    public User(Long id, String name, String surname, Date birthDay, Gender gender, Timestamp created, Timestamp changed, Float weight) {
+    public User(Long id, String name, String surname, Date birthDay,
+                String login, Float weight, boolean isDeleted,
+                Timestamp created, Timestamp changed) {
         this.id = id;
         this.name = name;
         this.surname = surname;
         this.birthDay = birthDay;
-        this.gender = gender;
+        this.login = login;
+        this.weight = weight;
+        this.isDeleted = isDeleted;
         this.created = created;
         this.changed = changed;
-        this.weight = weight;
     }
 
     public Long getId() {
@@ -65,12 +68,12 @@ public class User {
         this.birthDay = birthDay;
     }
 
-    public Gender getGender() {
-        return gender;
+    public String getLogin() {
+        return login;
     }
 
-    public void setGender(Gender gender) {
-        this.gender = gender;
+    public void setLogin(String login) {
+        this.login = login;
     }
 
     public Timestamp getCreated() {
@@ -97,29 +100,39 @@ public class User {
         this.weight = weight;
     }
 
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        isDeleted = deleted;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return Objects.equals(id, user.id) &&
-                Objects.equals(name, user.name) &&
-                Objects.equals(surname, user.surname) &&
-                Objects.equals(birthDay, user.birthDay) &&
-                gender == user.gender &&
-                Objects.equals(created, user.created) &&
-                Objects.equals(changed, user.changed) &&
-                Objects.equals(weight, user.weight);
+        return isDeleted == user.isDeleted
+                && Objects.equals(id, user.id)
+                && Objects.equals(name, user.name)
+                && Objects.equals(surname, user.surname)
+                && Objects.equals(birthDay, user.birthDay)
+                && Objects.equals(login, user.login)
+                && Objects.equals(weight, user.weight)
+                && Objects.equals(created, user.created)
+                && Objects.equals(changed, user.changed);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, surname, birthDay,
-                gender, created, changed, weight);
+        return Objects.hash(id, name, surname, birthDay, login,
+                weight, isDeleted, created, changed);
     }
 
     @Override
     public String toString() {
-        return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE);
+        return ToStringBuilder.reflectionToString(this,
+                ToStringStyle.JSON_STYLE);
     }
 }
