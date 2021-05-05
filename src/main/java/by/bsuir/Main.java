b237dacd4a87297ed1512b20f27bc7e9ede3d38a
Aleksandr Ahramovich <al-hrom-3@mail.ru>
2021-05-06 00:13:21 +0300

    add searchDealersForUser function

--- src/main/java/by/bsuir/Main.java
+++ src/main/java/by/bsuir/Main.java
@@ -25,82 +25,82 @@ public class Main {
         IUserRepository iUserRepository = new UserRepositoryImpl();
         IDealerRepository iDealerRepository = new DealerRepositoryImpl();
 
+        System.out.println();
+        System.out.println("//______________Find All______________________//");
+        System.out.println();
+
+        for (Dealer dealer : iDealerRepository.findAll()) {
+            System.out.println(dealer);
+        }
+
+        System.out.println();
+        System.out.println("//______________Find one______________________//");
+        System.out.println();
+
+        try {
+            System.out.println(iDealerRepository.findById(6L));
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
 //        System.out.println();
-//        System.out.println("//______________Find All______________________//");
-//        System.out.println();
-//
-//        for (Dealer dealer : iDealerRepository.findAll()) {
-//            System.out.println(dealer);
-//        }
-//
-//        System.out.println();
-//        System.out.println("//______________Find one______________________//");
-//        System.out.println();
-//
-//        try {
-//            System.out.println(iDealerRepository.findById(6L));
-//        } catch (Exception e) {
-//            System.err.println(e.getMessage());
-//        }
-//
-////        System.out.println();
-////        System.out.println("//______________Save one______________________//");
-////        System.out.println();
-////
-////        Dealer dealer = new Dealer();
-////        dealer.setName("Audi Авилон");
-////        dealer.setOpenDate(new Date(1_586_908_800_000L));
-////        dealer.setLocationDescription("Москва, ул. Автозаводская, д. 23, корп. 5");
-////        dealer.setLocationId(3L);
-////        dealer.setCreated();
-////        dealer.setChanged();
-////        dealer.setOpenHour(9);
-////        dealer.setCloseHour(21);
-//
-////
-////        System.out.println(iDealerRepository.save(dealer));
-//
-//        System.out.println();
-//        System.out.println("//______________Save dealer______________________//");
+//        System.out.println("//______________Save one______________________//");
 //        System.out.println();
 //
 //        Dealer dealer = new Dealer();
-//        dealer.setName("Test");
-//        dealer.setOpenDate(new Date(1_586_000_000_000L));
-//        dealer.setLocationDescription("Москва");
+//        dealer.setName("Audi Авилон");
+//        dealer.setOpenDate(new Date(1_586_908_800_000L));
+//        dealer.setLocationDescription("Москва, ул. Автозаводская, д. 23, корп. 5");
 //        dealer.setLocationId(3L);
 //        dealer.setCreated();
 //        dealer.setChanged();
 //        dealer.setOpenHour(9);
 //        dealer.setCloseHour(21);
-//
-//        dealer = iDealerRepository.save(dealer);
-//
-//        System.out.println(dealer);
-//
-//        System.out.println();
-//        System.out.println("//______________Update dealer______________________//");
-//        System.out.println();
-//
-//        dealer.setName("Test_after_update");
-//        dealer.setOpenDate(new Date(8_586_000_000_000L));
-//        dealer.setLocationDescription(null);
-//        dealer = iDealerRepository.update(dealer);
-//
-//        System.out.println(dealer);
-//
-//        System.out.println();
-//        System.out.println("//______________Delete dealer______________________//");
-//        System.out.println();
-//
-//        long id = dealer.getId();
-//        iDealerRepository.delete(dealer);
-//
-//        System.out.println();
-//        System.out.println("//______________Search dealer for user______________________//");
-//        System.out.println();
 
-        //User user = null;
+//
+//        System.out.println(iDealerRepository.save(dealer));
+
+        System.out.println();
+        System.out.println("//______________Save dealer______________________//");
+        System.out.println();
+
+        Dealer dealer = new Dealer();
+        dealer.setName("Test");
+        dealer.setOpenDate(new Date(1_586_000_000_000L));
+        dealer.setLocationDescription("Москва");
+        dealer.setLocationId(3L);
+        dealer.setCreated();
+        dealer.setChanged();
+        dealer.setOpenHour(9);
+        dealer.setCloseHour(21);
+
+        dealer = iDealerRepository.save(dealer);
+
+        System.out.println(dealer);
+
+        System.out.println();
+        System.out.println("//______________Update dealer______________________//");
+        System.out.println();
+
+        dealer.setName("Test_after_update");
+        dealer.setOpenDate(new Date(8_586_000_000_000L));
+        dealer.setLocationDescription(null);
+        dealer = iDealerRepository.update(dealer);
+
+        System.out.println(dealer);
+
+        System.out.println();
+        System.out.println("//______________Delete dealer______________________//");
+        System.out.println();
+
+        long id = dealer.getId();
+        iDealerRepository.delete(dealer);
+
+        System.out.println();
+        System.out.println("//______________Search dealer for user______________________//");
+        System.out.println();
+
+
         try {
             User user = iUserRepository.findById(10L);
             List<String> strDealers = new ArrayList<>(iDealerRepository.searchDealersForUser(user));
@@ -119,16 +119,16 @@ public class Main {
         System.out.println();
         System.out.println("//____________________________________//");
         System.out.println();
-//
-//        for (User user : iUserRepository.findAll()) {
-//            System.out.println(user);
-//        }
-//
-//        System.out.println(iUserRepository
-//                .findAll()
-//                .stream()
-//                .map(User::getName)
-//                .collect(Collectors.joining(", ")));
-//
+
+        for (User user : iUserRepository.findAll()) {
+            System.out.println(user);
+        }
+
+        System.out.println(iUserRepository
+                .findAll()
+                .stream()
+                .map(User::getName)
+                .collect(Collectors.joining(", ")));
+
     }
 }
